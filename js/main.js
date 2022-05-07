03cab18a55d9231322b445f74db02a00e1c939d3
Leonardo Kellner <leonardo.kellner@gmail.com>
2022-05-07 10:46:05 -0300

    muevo database a codigos.json y uso fetch con ruta relativa

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ let indice;
 let autorizaTiempo;
 
 // array de codigos
-const codigos = [];
+let codigos = [];
 
 //agregar resta de coseguros
 function restaCoseguro() {
@@ -34,12 +34,13 @@ function restaCoseguro() {
 
 // clase de codigos con metodos de cambio de datos
 class Prestacion {
-    constructor(codigo,descripcion,valor,valorCoseguro,coseguro){
+    constructor(codigo,descripcion,valor,valorCoseguro,coseguro,servicio){
         this.codigo = codigo;
         this.descripcion = descripcion;
         this.valor = valor;
         this.valorCoseguro = valorCoseguro;
         this.coseguro = coseguro;
+        this.servicio = servicio;
     }
     modificarValorManual(){
         this.valor = parseFloat(prompt("Ingrese el nuevo valor de la práctica"));
@@ -54,17 +55,44 @@ function addPrestacion(){ //pido a usuario datos para agregar codigos
     codigos.push(newPrestacion);
 }
 
+/* ========= MUEVO BASE DE DATOS A CODIGOS.JSON =========
+
 //creo codigos por defecto armando database
-const dbPrestacion = (codigo,descripcion,valor,valorCoseguro,coseguro) => { 
-    let newPrestacion = new Prestacion(codigo,descripcion,valor,valorCoseguro,coseguro);
+const dbPrestacion = (codigo,descripcion,valor,valorCoseguro,coseguro,servicio) => { 
+    let newPrestacion = new Prestacion(codigo,descripcion,valor,valorCoseguro,coseguro,servicio);
     codigos.push(newPrestacion);
 }
 
-dbPrestacion("180104","Ecografia Ginecológica",300,50,true);
-dbPrestacion("180106","Ecografia Mamaria",400,50,true);
-dbPrestacion("180112","Ecografia Abdominal",400,100,true);
-dbPrestacion("180116","Ecografia Renal",300,150,true);
+dbPrestacion("180104","Ecografia Ginecológica",300,50,true,18);
+dbPrestacion("180106","Ecografia Mamaria",400,50,true,18);
+dbPrestacion("180107","Ecografia Cerebral",400,50,true,18);
+dbPrestacion("180108","Ecografia Oftalmica",350,50,true,18);
+dbPrestacion("180110","Ecografia Tiroidea",300,50,true,18);
+dbPrestacion("180111","Ecografia Testicular",500,50,true,18);
+dbPrestacion("180112","Ecografia Abdominal",450,100,true,18);
+dbPrestacion("180113","Ecografia Hepatica, Biliar, Esplenica o Toracica",400,100,true,18);
+dbPrestacion("180114","Ecografia Vesicoprostatica",350,100,true,18);
+dbPrestacion("180116","Ecografia Renal",350,150,true,18);
+dbPrestacion("180117","Ecografia de Aorta Abdominal",300,150,true,18);
+dbPrestacion("180118","Ecografia Pancreatica o Suprarrenal",300,150,true,18);
+
+dbPrestacion("180202","Eco Doppler Mamario Bilateral",300,150,true,19);
+dbPrestacion("180204","Eco Doppler Cardiaco",500,50,true,19);
+dbPrestacion("180206","Eco Doppler Vascular Periferico",400,50,true,19);
+dbPrestacion("180208","Eco Doppler Abdominal",300,50,true,19);
+dbPrestacion("180210","Eco Doppler Arterial Bilateral",400,50,true,19);
+dbPrestacion("180214","Eco Doppler Venoso Bilateral",400,50,true,19);
+dbPrestacion("180218","Eco Doppler Vasos de Cuello",400,50,true,19);
+dbPrestacion("180224","Eco Doppler Testicular",300,50,true,19);
+dbPrestacion("180226","Eco Doppler Obstetrico",300,50,true,19);
+dbPrestacion("180228","Eco Doppler Cardiaco Fetal",500,50,true,19);
+dbPrestacion("180239","Eco Doppler Cardiaco Transesofagico",500,50,true,19);
+dbPrestacion("180230","Eco Doppler Transcraneano",500,50,true,19);
+dbPrestacion("180240","Eco Doppler Aorta Renal",300,50,true,19);
+dbPrestacion("180248","Eco Doppler Tiroideo",300,50,true,19);
+
 //fin de database
+*/
 
 const cambioLogin = (element) =>{
     let form = document.querySelector(".login");
@@ -93,6 +121,7 @@ loginBtn.onclick = (e) => {
     cambioLogin(usuario.value);
     localStorage.setItem("user",usuario.value);
     cambioMain();
+    obtenerCodigos();
 }
 
 const logout = () =>{
@@ -103,6 +132,7 @@ const logout = () =>{
 ctaAutorizar.onclick = (e) => {
     e.preventDefault();
     cambioMain();
+    obtenerCodigos();
 }
 
 
@@ -168,11 +198,21 @@ const clearStorage = (key) =>{
 const mostrarToast = () => {
     setTimeout( ()=>{
         Toastify({
-            text: "Calculo guardado en servidor",
+            text: "Calculo realizado correctamente",
             duration: 4000,
             style: {
                 background: "linear-gradient(to top, #00ff76, #84ecb4)"
             }
         }).showToast()
     },1000)
+}
+
+const obtenerCodigos = async () => {
+    try {
+        let respuesta = await fetch("js/codigos.json");
+        let resultado = await respuesta.json();
+        codigos = resultado;
+    } catch (error) {
+        console.log(error);
+    }
 }
